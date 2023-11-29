5a7f9773d3a56b16adbf850b95494fa648d2808e
Benjamin-home <benjamin.valleran@hotmail.fr>
2023-11-29 19:01:25 +0100

    Ajout des fonctions popup et de css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
 const submit = document.querySelector("#submit");
+const dismiss = document.querySelector("#dismiss");
 const email = document.querySelector("#sign .email");
 const alerte = document.querySelector("#sign #alerte");
 const span = document.querySelector("#mailUser");
 
+/*Fonction qui affiche le popup*/
+afficherPopup = ()=>{
+    let popupBackground = document.querySelector(".popupBackground")
+    // La popup est masquée par défaut (display:none), ajouter la classe "active"
+    // va changer son display et la rendre visible. 
+    popupBackground.classList.add("active")
+}
+
+/*Fonction qui cache le popup*/
+cacherPopup=()=>{
+    let popupBackground = document.querySelector(".popupBackground")
+    // La popup est masquée par défaut (display:none), supprimer la classe "active"
+    // va rétablir cet affichage par défaut. 
+    popupBackground.classList.remove("active")
+}
 
 submit.addEventListener("click", (event)=>{
     let mailUser = email.value;
-    if(email.value.includes("@") && email.value.includes(".fr") || email.value.includes(".com")){
-        mailUser = email.value;
-        submit.href = "./succes.html";
+    if(mailUser.includes("@") && mailUser.includes(".fr") || mailUser.includes(".com")){
+        span.innerHTML = mailUser;
+        afficherPopup();
     } else {
         email.classList.add("alerte");
         alerte.classList.add("show");
@@ -16,6 +32,8 @@ submit.addEventListener("click", (event)=>{
         email.value = email.value;
         event.preventDefault();
     }
-    console.dir(mailUser);
-    span.append(mailUser);
+})
+
+dismiss.addEventListener("click", ()=>{
+    cacherPopup();
 })
